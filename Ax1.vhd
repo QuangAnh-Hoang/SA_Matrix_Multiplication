58445521d068113f9f90e428cc87f32c05e56e1c
Quang Anh Hoang <47719192+QuangAnh-Hoang@users.noreply.github.com>
2021-04-28 00:40:32 -0400

    Update Ax1.vhd

--- Ax1.vhd
+++ Ax1.vhd
@@ -66,65 +66,36 @@ end component;
 
 -- Variables --
 type two_d_array is array (natural range <>, natural range <>) of std_logic; 
-signal ci, di, carry_i, psi: two_d_array(7 downto 0, 7 downto 0);
+signal c, d, carry, ps: two_d_array(7 downto 0, 7 downto 0);
 
 begin
 
 INIT_C: for j in 0 to 7 generate
-    ci(0, j) <= i_c(j);
+    c(0, j) <= i_c(j);
+    c(4, j) <= i_c(j);
 end generate INIT_C;
 
 INIT_D: for i in 0 to 7 generate
-    di(i, 0) <= i_d(i);
-    carry_i(i, 0) <= '0';
+    d(i, 0) <= i_d(i);
+    carry(i, 0) <= '0';
 end generate INIT_D;
 
-ROW: for i in 0 to 3 generate
-    COL: for j in 0 to 7 generate
-        
-        -- RHS
-        ROW_0: if i = 0 and j < 7 generate
-            di(i, j+1) <= ci(i, j) and di(i, j);
-            ci(i+1, j) <= ci(i, j) and di(i, j);
-        end generate ROW_0;
-
-        ROW_0_5to0: if i = 0 and j < 6 generate
-            psi(i+1,j) <= ci(i, j+1) and di(i, j+1);
-        end generate ROW_0_5to0;
-        
-        psi(1,6) <= ci(0, 7) nand di(0, 7);
-        psi(1,7) <= '1';
+AND_ROW: for j in 0 to 6 generate
+    c(1, j) <= c(0, j) and d(0, j);
+    d(0, j+1) <= d(0, j);
     
-        ROW_0_7: if i = 0 and j = 7 generate
-            ci(i+1, j) <= ci(i, j) nand di(i, j);
-        end generate ROW_0_7;
-        
-        ROW_PPUx_RHS: if i /= 0 and i+j <= 3 generate
-            PPU_X: PPUx port map(
-                i_ci => ci(i, j),
-                i_dj => di(i, j),
-                i_carry => carry_i(i, j),
-                i_sum => psi(i, j),
-                o_ci => ci(i+1, j),
-                o_dj => di(i, j+1),
-                o_carry => carry_i(i, j+1),
-                o_sum => psi()
-            );
-        end generate ROW_PPUx_RHS;
-        
-    end generate COL;
+    c(5, j) <= c(0, j) and d(4, j);
+    d(4, j+1) <= d(4, j);
+end generate AND_ROW;
 
-    COL_L: for j in 0 to 3 generate        
-        -- LHS
-        ROW_4: if i = 4 and j < 7 generate
-            di(i, j+1) <= ci(i, j) and di(i, j);
-            ci(i+1, j) <= ci(i, j) and di(i, j);
-        end generate ROW_4;
-        
-        ROW_4_7: if i = 4 and j = 7 generate
-            ci(i+1, j) <= ci(i, j) nand di(i, j);
-        end generate ROW_4_7;
-        
-    end generate COL_L;        
-end generate ROW;
+c(1, 7) <= c(0, 7) nand d(0,7);
+c(5, 7) <= c(4, 7) nand d(4,7);
+
+COL_PPUx: for j in 0 to 3 generate
+    ROW_PPUx: for i in 1 to 3-j generate
+
+    end generate ROW_PPUx;
+end generate COL_PPUx;
+
+
 end Structural;
