b4f71770cd662cc9d5b0361474311888f34de94a
Quang Anh Hoang <47719192+QuangAnh-Hoang@users.noreply.github.com>
2021-05-04 01:42:51 -0400

    Start with conventional Multiplier architecture

--- Ax1.vhd
+++ Ax1.vhd
@@ -2,7 +2,7 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 04/18/2021 10:42:04 PM
+-- Create Date: 05/03/2021 09:07:37 PM
 -- Design Name: 
 -- Module Name: Ax1 - Structural
 -- Project Name: 
@@ -48,54 +48,123 @@ component PPU is
     );
 end component;
 
-component PPUx is
-    port(
-        i_ci, i_dj, i_carry, i_sum: in std_logic;
-        o_ci, o_dj, o_carry, o_sum: out std_logic
-    );
-end component;
-
-component CLA is
-    generic(n: natural:= 8);
-    port(
-        i_a, i_b: in std_logic_vector(n-1 downto 0);
-        o_sum: out std_logic_vector(n-1 downto 0);
-        o_carry: out std_logic
-    );
-end component;
-
 -- Variables --
 type two_d_array is array (natural range <>, natural range <>) of std_logic; 
-signal c, d, carry, ps: two_d_array(7 downto 0, 7 downto 0);
+signal ci, di, psi, cri: two_d_array(7 downto 0, 7 downto 0);
+signal p: std_logic_vector(15 downto 0);
+signal cout: std_logic;
 
 begin
 
 INIT_C: for j in 0 to 7 generate
-    c(0, j) <= i_c(j);
-    c(4, j) <= i_c(j);
+    ci(0, j) <= i_c(j);
+    psi(0, j) <= '0';
 end generate INIT_C;
 
 INIT_D: for i in 0 to 7 generate
-    d(i, 0) <= i_d(i);
-    carry(i, 0) <= '0';
+    di(i, 0) <= i_d(i);
+    cri(1, 0) <= '0';
 end generate INIT_D;
 
-AND_ROW: for j in 0 to 6 generate
-    c(1, j) <= c(0, j) and d(0, j);
-    d(0, j+1) <= d(0, j);
-    
-    c(5, j) <= c(0, j) and d(4, j);
-    d(4, j+1) <= d(4, j);
-end generate AND_ROW;
+GY: for i in 0 to 7 generate
+    GX: for j in 0 to 7 generate 
 
-c(1, 7) <= c(0, 7) nand d(0,7);
-c(5, 7) <= c(4, 7) nand d(4,7);
+        G1: if i=0 and j<7 and j>0 generate
+            ci(i+1, j) <= ci(i, j);
+            di(i, j+1) <= di(i, j);
+            psi(i+1, j-1) <= ci(i, j) and di(i, j);
+        end generate G1;
+        
+        ci(1, 0) <= ci(0, 0);
+        di(0, 1) <= di(0, 0);
+        p(0) <= ci(0, 0) and di(0, 0);
+        
+        ci(1, 7) <= ci(0, 7);
+        psi(1, 6) <= ci(0, 7) nand di(0, 7);
+        
+        G2: if i>0 and i<7 and j=0 generate
+            ELM: PPU port map (
+                i_ci => ci(i, j),
+                i_dj => di(i, j),
+                i_carry => cri(i, j),
+                i_sum => psi(i, j),
+                o_ci => ci(i+1, j),
+                o_dj => di(i, j+1),
+                o_carry => cri(i, j+1),
+                o_sum => p(i)
+            );
+        end generate G2;
+        
+        G3: if i=7 and j<7 generate
+            ELM: PPU port map (
+                i_ci => ci(i, j),
+                i_dj => di(i, j),
+                i_carry => cri(i, j),
+                i_sum => psi(i, j),
+                o_ci => open,
+                o_dj => di(i, j+1),
+                o_carry => cri(i, j+1),
+                o_sum => p(i+j)
+            );
+        end generate G3;
 
-COL_PPUx: for j in 0 to 3 generate
-    ROW_PPUx: for i in 1 to 3-j generate
+        G4: if i<7 and i>0 and j<7 and j>0 generate
+            ELM: PPU port map (
+                i_ci => ci(i, j),
+                i_dj => di(i, j),
+                i_carry => cri(i, j),
+                i_sum => psi(i, j),
+                o_ci => ci(i+1, j),
+                o_dj => di(i, j+1),
+                o_carry => cri(i, j+1),
+                o_sum => psi(i+1, j-1)
+            );
+        end generate G4;
 
-    end generate ROW_PPUx;
-end generate COL_PPUx;
+        G5: if i=1 and j=7 generate
+            ELM: PPU port map (
+                i_ci => ci(i, j),
+                i_dj => di(i, j),
+                i_carry => cri(i, j),
+                i_sum => '1',
+                o_ci => ci(i+1, j),
+                o_dj => open,
+                o_carry => psi(i+1, j),
+                o_sum => psi(i+1, j-1)
+            );
+        end generate G5;
 
+        G6: if i>1 and i<7 and j=7 generate
+            ELM: PPU port map (
+                i_ci => ci(i, j),
+                i_dj => di(i, j),
+                i_carry => cri(i, j),
+                i_sum => psi(i, j),
+                o_ci => ci(i+1, j),
+                o_dj => open,
+                o_carry => psi(i+1, j),
+                o_sum => psi(i+1, j-1)
+            );
+        end generate G6;
+
+        G7: if i=7 and j=7 generate
+            ELM: PPU port map (
+                i_ci => ci(i, j),
+                i_dj => di(i, j),
+                i_carry => cri(i, j),
+                i_sum => psi(i, j),
+                o_ci => open,
+                o_dj => open,
+                o_carry => cout,
+                o_sum => p(i+j)
+            );
+        end generate G7;
+
+        p(15) <= cout xor '1';
+
+    end generate GX;
+end generate GY;
+
+o_eout <= p(15 downto 0);
 
 end Structural;
